c6674854872cc6b4cf4cdd9c28d145ee831eba4c
EmirParadi <emir.alejandro.paradi@gmail.com>
2023-10-21 18:18:56 -0300

    ejercicio3 terminado

---
+++ js/ejercicio3.js
@@ -0,0 +1,4 @@
+/* 3.- Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
+ */
+
+document.write("<b>El resutaldo de sumar 2 + 3 es igual a: </b>" + (2 + 3));
